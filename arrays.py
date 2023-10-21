783f42b2e07fc90c80f31db57d369cbe6adeeda0
Duke Nyamongo (Lester) <dukelester4@gmail.com>
2023-10-21 13:40:51 +0300

    Check if the array has been rotated and sorted

--- arrays.py
+++ arrays.py
@@ -20,3 +20,16 @@ print(my_mult_dim, my_mult_dim.shape)
 print(np.arange(0, 200,50, dtype=np.float32))
 print(np.linspace(200, 3000, 200000000, dtype=np.int32))
 print(np.empty([3, 4], dtype=np.int64))
+
+def check_circular_sorted(arr):
+    ''' Array is sorted and rotated'''
+    count = 0
+    n = len(arr)
+    for i in range(1, n):
+        if arr[i - 1] > arr[i]:
+            count += 1
+    if count == 1:
+        return arr[0] > arr[n - 1]
+    return False
+arr = [23, 34, 45, 12, 17, 19]
+print(check_circular_sorted(arr))
