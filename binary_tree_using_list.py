8e2b70b7d5599f7fae3505a148f2b8503e04ee36
Duke Nyamongo (Lester) <dukelester4@gmail.com>
2023-10-27 14:48:55 +0300

    Added a function to search for an element in a binary tree

--- binary_tree_using_list.py
+++ binary_tree_using_list.py
@@ -12,4 +12,25 @@ class BinaryTree:
         self.last_index_used = 0
         self.max_size = size
 
+    # Insert into a Binary tree
+
+    def insert_node(self, new_node):
+        ''' Insert the new node to the binary Tree'''
+        if self.last_index_used + 1 == self.max_size:
+            return 'The tree is full'
+        self.tree_list[self.last_index_used + 1] = new_node
+        self.last_index_used += 1
+        return 'The value was successfully added!'
+
+    def search_for_node(self, node_value):
+        '''Search for an element in a node '''
+        for i, _ in enumerate(self.tree_list):
+            if self.tree_list[i] == node_value:
+                return 'Element Found'
+        return 'Element can not be found'
+
+
 binary_tree = BinaryTree(10) # Creating O(N) for space and O(1) for time
+print(binary_tree.insert_node(34), binary_tree.insert_node(44), binary_tree.insert_node(30)) # O(1)
+print(binary_tree.search_for_node(440))
+print(binary_tree.search_for_node(44))
